d35154c90110f8ff6dba3327038fe744c97fd85a
SimKien <simonkienle@web.de>
2023-10-23 14:29:21 +0200

    create board and add chess pieces

--- frontend/my-webapp/src/pages/chess/Board.tsx
+++ frontend/my-webapp/src/pages/chess/Board.tsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
 import "./Board.css";
 
 export default function Board() {
+    const size = 8;
+    const [board, setBoard] = useState(new Array<Array<string>>());
+
+    useEffect(() => {
+        const result = new Array<Array<string>>(size);
+        for (let i: number = 0; i < size; i++) {
+            result[i] = new Array<string>(size);
+            for (let j: number = 0; j < size; j++) {
+                result[i][j] = "";
+            }
+        }
+        setBoard(result);
+    }, []);
+
     return(
-        <div></div>
+        <div className="board">
+            {
+                board.map((row, rindex) => {
+                    return <div className="row">{
+                        row.map((_, cindex) => {
+                            return <div className={`square ${(rindex + cindex) % 2 === 0 ? "white" : "black"}`}></div>;
+                        })
+                    }</div>;
+                })
+            }
+        </div>
     );
 }
--- frontend/my-webapp/src/pages/chess/chess.tsx
+++ frontend/my-webapp/src/pages/chess/chess.tsx
@@ -3,8 +3,7 @@ import Board from "./Board";
 export default function Chess() {
     return (
         <div>
-            <h1>Chess</h1>
-            <p>Coming soon...</p>
+            <Board />
         </div>
     );
 }
