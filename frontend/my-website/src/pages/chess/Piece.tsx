1abb8a7780e2a5a7f36dca6bde9b1dd5b3002225
SimKien <simonkienle@web.de>
2023-10-25 13:58:57 +0200

    Replace vite-plugin-svg

--- frontend/my-website/src/pages/chess/Piece.tsx
+++ frontend/my-website/src/pages/chess/Piece.tsx
@@ -1,15 +1,15 @@
-import KingWhiteSVG from "assets/kingwhite.svg?react";
-import KingBlackSVG from "assets/kingblack.svg?react";
-import QueenWhiteSVG from "assets/queenwhite.svg?react";
-import QueenBlackSVG from "assets/queenblack.svg?react";
-import RookWhiteSVG from "assets/rookwhite.svg?react";
-import RookBlackSVG from "assets/rookblack.svg?react";
-import KnightWhiteSVG from "assets/knightwhite.svg?react";
-import KnightBlackSVG from "assets/knightblack.svg?react";
-import BishopWhiteSVG from "assets/bishopwhite.svg?react";
-import BishopBlackSVG from "assets/bishopblack.svg?react";
-import PawnWhiteSVG from "assets/pawnwhite.svg?react";
-import PawnBlackSVG from "assets/pawnblack.svg?react";
+import KingWhiteSVG from "assets/kingwhite.svg";
+import KingBlackSVG from "assets/kingblack.svg";
+import QueenWhiteSVG from "assets/queenwhite.svg";
+import QueenBlackSVG from "assets/queenblack.svg";
+import RookWhiteSVG from "assets/rookwhite.svg";
+import RookBlackSVG from "assets/rookblack.svg";
+import KnightWhiteSVG from "assets/knightwhite.svg";
+import KnightBlackSVG from "assets/knightblack.svg";
+import BishopWhiteSVG from "assets/bishopwhite.svg";
+import BishopBlackSVG from "assets/bishopblack.svg";
+import PawnWhiteSVG from "assets/pawnwhite.svg";
+import PawnBlackSVG from "assets/pawnblack.svg";
 
 export type Position = [number, number];
 export type PieceType = "K" | "Q" | "R" | "B" | "N" | "P";
@@ -25,12 +25,12 @@ export interface Piece {
 }
 
 export const PIECE_MAP: PieceMap= {
-    K: (color: PieceColor) => color === 'white' ? <KingWhiteSVG /> : <KingBlackSVG />,
-    Q: (color: PieceColor) => color === 'white' ? <QueenWhiteSVG /> : <QueenBlackSVG />,
-    R: (color: PieceColor) => color === 'white' ? <RookWhiteSVG /> : <RookBlackSVG />,
-    B: (color: PieceColor) => color === 'white' ? <BishopWhiteSVG /> : <BishopBlackSVG />,
-    N: (color: PieceColor) => color === 'white' ? <KnightWhiteSVG /> : <KnightBlackSVG />,
-    P: (color: PieceColor) => color === 'white' ? <PawnWhiteSVG /> : <PawnBlackSVG />
+    K: (color: PieceColor) => color === 'white' ? <img src={KingWhiteSVG} alt="King White"></img> : <img src={KingBlackSVG} alt="King Black"></img>,
+    Q: (color: PieceColor) => color === 'white' ? <img src={QueenWhiteSVG} alt="Queen White"></img> : <img src={QueenBlackSVG} alt="Queen Black"></img>,
+    R: (color: PieceColor) => color === 'white' ? <img src={RookWhiteSVG} alt="Rook White"></img> : <img src={RookBlackSVG} alt="Rook Black"></img>,
+    B: (color: PieceColor) => color === 'white' ? <img src={BishopWhiteSVG} alt="Bishop White"></img> : <img src={BishopBlackSVG} alt="Bishop Black"></img>,
+    N: (color: PieceColor) => color === 'white' ? <img src={KnightWhiteSVG} alt="Knight White"></img> :<img src={KnightBlackSVG} alt="Knight Black"></img>,
+    P: (color: PieceColor) => color === 'white' ? <img src={PawnWhiteSVG} alt="Pawn White"></img> : <img src={PawnBlackSVG} alt="Pawn Black"></img>
 };
 
 export function PieceComponent(props: {piece: Piece | undefined}) {
