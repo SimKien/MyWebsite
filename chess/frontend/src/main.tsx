6ebbd83b47d008ac0bdea4513c5279f75f1e18c7
https://git.scc.kit.edu/ukxiv <simon.kienle@student.kit.edu>
2023-11-25 15:29:30 +0100

    add touch dnd preview

--- chess/frontend/src/main.tsx
+++ chess/frontend/src/main.tsx
@@ -5,14 +5,32 @@ import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { isMobile } from 'react-device-detect';
 import { TouchBackend } from 'react-dnd-touch-backend';
+import { usePreview } from 'react-dnd-preview';
+import { Piece } from 'chess/lib/constants/ChessConstants';
+
+import "chess/style/Piece.css"
+import { PIECE_MAP } from 'chess/components/Piece';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const MobilePreview = () => {
+  const preview = usePreview();
+  if (!preview.display) {
+    return null;
+  }
+  const { item, style } = preview;
+  const piece = item as Piece
+  return <div style={{ ...style }}>{PIECE_MAP[piece.type](piece.color, "1.0")}</div>
+}
+
 let chess = isMobile ?
-  <DndProvider backend={TouchBackend} >
+  <DndProvider backend={TouchBackend}>
     <Chess />
-  </DndProvider >
+    <MobilePreview />
+  </DndProvider>
   :
   <DndProvider backend={HTML5Backend}>
     <Chess />
