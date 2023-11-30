685c0e5a70cf41e360bbe1768d504b5796a3db1e
https://git.scc.kit.edu/ukxiv <simon.kienle@student.kit.edu>
2023-11-30 19:44:40 +0100

    Change name of Env variable to TICTACTOE_PORT

--- tictactoe/backend/src/main.rs
+++ tictactoe/backend/src/main.rs
@@ -12,7 +12,7 @@ async fn main() {
                 .fallback(ServeFile::new(&format!("{}/index.html", FRONTEND_PATH))),
         ),
     );
-    let port = std::env::var("CHESS_PORT").unwrap_or_else(|_| "8080".to_string());
+    let port = std::env::var("TICTACTOE_PORT").unwrap_or_else(|_| "8080".to_string());
 
     axum::Server::bind(&format!("0.0.0.0:{}", port).parse().unwrap())
         .serve(app.into_make_service())
